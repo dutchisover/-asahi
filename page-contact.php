7c0fce48ef4fdfa463b2241c4700d27693bc58db
hkuniki <h.kuniki.1211@gmail.com>
2024-07-26 13:35:46 +0900

    お問い合わせフォームひとまず作成

---
+++ page-contact.php
@@ -0,0 +1,86 @@
+<?php get_header(); ?>
+
+<?php
+// ページのスラッグを取得
+global $post;
+$slug = $post->post_name;
+?>
+
+<div class="page__header">
+    <div class="page__mv">
+        <h1 class=" page__title">
+            <em><?= get_the_title(); ?></em>
+            <span><?= get_field("page-sub-title") ?></span>
+        </h1>
+
+        <picture>
+            <source media=" (max-width:820px)" srcset="<?= get_template_directory_uri(); ?>/assets/image/bg_page-mv-service_sp.jpg">
+            <img src="<?= get_template_directory_uri(); ?>/assets/image/bg_page-mv-service.jpg" alt="" class="page__mv-bg" width="1800" height="502">
+        </picture>
+    </div>
+    <!-- /.page__mv -->
+
+    <div class="breadcrumb">
+        <div class="breadcrumb__inner">
+            <a href="<?php echo home_url(); ?>" class="breadcrumb__link">トップ</a>
+            <span class="breadcrumb__current"><?= get_field("page-sub-title") ?></span>
+        </div>
+    </div>
+    <!-- /.breadcrumb -->
+</div>
+
+<main class="main page__main">
+    <?php
+    echo '<div class="page-' . esc_attr($slug) . '">';
+    ?>
+
+    <section class="section" data-section-title="contact">
+
+        <p>初回30分 相談無料</p>
+        <p>お気軽にご相談ください！</p>
+        <p class="section__copy">
+            法務に関わるご相談・お問い合わせは、お電話またはメールにて承っています。
+            法律相談をご希望される場合は、初めに「法律相談を希望します」とお伝えください。
+            コンタクトフォームでのお問い合わせは24時間年中お受けしております。
+        </p>
+
+        <div class="contact__content">
+            <div>お電話でのお問い合わせ</div>
+            <div>
+                <div>072-627-0101</div>
+                <div>受付時間 9 : 00 - 18:00 (土日祝休)</div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="section" data-section-title="Flow" id="anc-flow">
+        <h2 class="section__title">
+            <em>contact form</em>
+            <span>お問い合わせフォームはこちら</span>
+        </h2>
+
+        <p class="section__copy">
+            48時間以内に連絡がない場合、メールがブロックされている恐れがありますので、
+            072-627-0101まで直接お電話いただくか、
+            info@ibarakiasahilaw.comへ直接メールにてご連絡をお願いいたします。
+        </p>
+        <p class="section__copy">
+            ※ メールのみでの法律相談はお受けしておりませんのでご承知おきください。
+        </p>
+
+        <div class="contact__form">
+            <?php echo do_shortcode('[contact-form-7 id="8bbf352" title="お問い合わせフォーム"]'); ?>
+        </div>
+
+    </section>
+
+    <?php
+    echo '</div>';
+    ?>
+
+</main>
+<!-- /.main -->
+
+<?php get_footer(); ?>
