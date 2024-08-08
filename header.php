6c6ebbdbf1fa4192cfd1575ca3fb17d7521e37c2
hkuniki <h.kuniki.1211@gmail.com>
2024-08-08 16:17:42 +0900

    gtag挿入

--- header.php
+++ header.php
@@ -4,7 +4,24 @@
 	<head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	    <!-- Google Tag Manager -->
+	    <script>
+	        (function(w, d, s, l, i) {
+	            w[l] = w[l] || [];
+	            w[l].push({
+	                'gtm.start': new Date().getTime(),
+	                event: 'gtm.js'
+	            });
+	            var f = d.getElementsByTagName(s)[0],
+	                j = d.createElement(s),
+	                dl = l != 'dataLayer' ? '&l=' + l : '';
+	            j.async = true;
+	            j.src =
+	                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+	            f.parentNode.insertBefore(j, f);
+	        })(window, document, 'script', 'dataLayer', 'GTM-NQGFXT8M');
+	    </script>
+	    <!-- End Google Tag Manager -->
 	    <!-- Web Fonts -->
 	    <script type="text/javascript" src="//typesquare.com/3/tsst/script/ja/typesquare.js?5ff66db6807442a0aa8a3222e90393a3" charset="utf-8"></script>
 
@@ -14,6 +31,9 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+	    <!-- Google Tag Manager (noscript) -->
+	    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQGFXT8M" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	    <!-- End Google Tag Manager (noscript) -->
 
 	    <?php get_template_part('inc/inc-tag-body'); ?>
 
