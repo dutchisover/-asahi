458416b0b3488eff64151f22aac8fea056bb1673
hkuniki <h.kuniki.1211@gmail.com>
2024-08-20 13:53:05 +0900

    キャッシュ値更新

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function add_files()
     //キャッシュ無効（開発時はこちらをコメント解除）
     // $cache = date('YmdHis');
     //キャッシュ有効（公開後はこちらをコメント解除）
-    $cache = 1.12;
+    $cache = 1.13;
 
     // WordPress提供のjquery.jsを読み込まない
     wp_deregister_script('jquery');
